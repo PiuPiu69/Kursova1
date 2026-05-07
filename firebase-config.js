82fc7199f17b39d57616ac13ba505c33792187e0
PiuPiu69 <163861374+PiuPiu69@users.noreply.github.com>
2026-05-07 23:05:22 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 
 
 export const firebaseConfig = {
-    apiKey : "AIzaSyA4nhImieNi7fDV8axuBHiq0ksbcfGjZzo" , 
+  apiKey : "AIzaSyA4nhImieNi7fDV8axuBHiq0ksbcfGjZzo" , 
   authDomain : "kursova1-d43e4.firebaseapp.com" , 
-  идентификатор проекта : "kursova1-d43e4" , 
+  projectId : "kursova1-d43e4" , 
   storageBucket : "kursova1-d43e4.firebasestorage.app" , 
   messagingSenderId : "88516887260" , 
   appId : "1:88516887260:web:045e402ba51e2ae48aaa19" 
