ed245ef8ff214966c9f8e21eff43075055c5d148
PiuPiu69 <163861374+PiuPiu69@users.noreply.github.com>
2026-05-07 22:33:55 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+
+export const firebaseConfig = {
+    apiKey: "ВАШ_API_KEY",
+    authDomain: "ВАШ_AUTH_DOMAIN",
+    projectId: "ВАШ_PROJECT_ID",
+    storageBucket: "ВАШ_STORAGE_BUCKET",
+    messagingSenderId: "ВАШ_SENDER_ID",
+    appId: "ВАШ_APP_ID"
+};
