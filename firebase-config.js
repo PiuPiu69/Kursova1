46f5e471215a3dccf3929222bf9c0687a61214f8
PiuPiu69 <163861374+PiuPiu69@users.noreply.github.com>
2026-05-07 23:12:25 +0300

    Update Firebase configuration with new credentials

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,8 @@
-
-
-export const firebaseConfig = {
-  apiKey : "AIzaSyA4nhImieNi7fDV8axuBHiq0ksbcfGjZzo" , 
-  authDomain : "kursova1-d43e4.firebaseapp.com" , 
-  projectId : "kursova1-d43e4" , 
-  storageBucket : "kursova1-d43e4.firebasestorage.app" , 
-  messagingSenderId : "88516887260" , 
-  appId : "1:88516887260:web:045e402ba51e2ae48aaa19" 
+const firebaseConfig = {
+  apiKey: "AIzaSyDu4HEpjWVfJOE4GMji2ux-KK_47-YRvfw",
+  authDomain: "kursova1-4e839.firebaseapp.com",
+  projectId: "kursova1-4e839",
+  storageBucket: "kursova1-4e839.firebasestorage.app",
+  messagingSenderId: "580567528040",
+  appId: "1:580567528040:web:81c77c2a05bf9c183841dd"
 };
